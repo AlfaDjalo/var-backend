3afdd2ae40d0c7a3824143ef2c59db01fa060f89
David Collier <david_a_collier@hotmail.com>
2026-01-18 15:21:27 +1100

    Forgot one file...

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",  # dev server
-        "https://your-username.github.io",  # your GitHub Pages production URL (no trailing slash)
+        "https://AlfaDjalo.github.io",  # your GitHub Pages production URL (no trailing slash)
     ],
     allow_credentials=True,
     allow_methods=["*"],
